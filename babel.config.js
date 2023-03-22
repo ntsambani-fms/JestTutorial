80bba6cb726bf75931eaa3f6cf4e1d8312d625ca
ntsambani-fms <ntsambani@fmsystems.com>
2023-03-22 11:08:17 +0200

    first commit

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  // presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  // presets: [
+  //   ['@babel/preset-env', {targets: {node: 'current'}}],
+  //   '@babel/preset-typescript',
+  // ],
+};
